f3b61f42d923717b15577ce25615f4c64bc986ce
dcamachog1501 <dcamachog1501@gmail.com>
2018-03-08 16:32:26 -0600

    Check2

--- src/Ventanas/Gestor.java
+++ src/Ventanas/Gestor.java
@@ -3,20 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Main;
+package Ventanas;
 
 /**
  *
  * @author dcama
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-        
-    }
+public class Gestor {
     
 }
