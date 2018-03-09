b3d1fe71b03d62067a2072b9b694bbbdde19386e
dcamachog1501 <dcamachog1501@gmail.com>
2018-03-08 23:58:21 -0600

    Titulo

--- src/Ventanas/Gestor.java
+++ src/Ventanas/Gestor.java
@@ -5,10 +5,19 @@
  */
 package Ventanas;
 
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
-import javax.imageio.ImageIO;
+import java.io.File;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
@@ -20,16 +29,22 @@ public class Gestor
    private final String Icono="";
    private int Largo;
    private int Ancho;
-   
-public Gestor()
-{
- 
-}
-public Gestor(int Largo,int Ancho)
-{
-    this.Largo=Largo;
-    this.Ancho=Ancho;
-}
+   private  Font FuenteTitulo;
+
+
+
+
+
+    public Gestor()
+    {
+        try {
+        FuenteTitulo = Font.createFont(Font.TRUETYPE_FONT, new File("Resources/Fuentes/Furore.ttf")).deriveFont(Font.PLAIN,99);
+        } catch(Exception e) {
+            FuenteTitulo = null;
+        }
+    }
+
+
 /**
  * Metodo encargado de determinar cual ventana se despliega
  * @param x Numero de ventana a desplegar
@@ -44,17 +59,42 @@ public void GestWindow(int x)
         }
     }
 }
+
+
 /**
  * Metodo que crea la ventana inicial.
  */
 public void Inicial_Creator()
 {
+  
+    
   JFrame Ventana = new JFrame(Titulo);
   Ventana.setResizable(false);
   Ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   Ventana.setIconImage(Toolkit.getDefaultToolkit().getImage("Resources/Icono.png"));
-  Ventana.setSize((Ventana_Inicial.vent.getLargo()),(Ventana_Inicial.vent.getAncho()));
+  Ventana.setSize(800,700);
+  Ventana.setLocationRelativeTo(Ventana);
+  Ventana.getContentPane().setBackground(Color.DARK_GRAY);
   Ventana.setVisible(true);
+  
+  JPanel Posterior= new JPanel();
+  Posterior.setBackground(Color.DARK_GRAY);
+  Posterior.setLayout(new GridBagLayout());
+  GridBagConstraints lbl1= new GridBagConstraints();
+  Ventana.add(Posterior);
+  
+  JLabel Title= new JLabel("Invaders");
+  Title.setFont(FuenteTitulo);
+  Title.setForeground(Color.GREEN);
+  lbl1.gridx=100;
+  lbl1.gridy=200;
+  Posterior.add(Title,lbl1);
+  
+  
+  
+  JLabel Icon=new JLabel();
+  Icon.setIcon(new ImageIcon(("Resources/Icono.png")));
+  //Ventana.add(Icon);
 }
 /**
  * Metodo que retorna el valor del ancho de una ventana
