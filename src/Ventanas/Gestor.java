a643fff7744e48ddb8b6778371a18c6a55d84ba3
dcamachog1501 <dcamachog1501@gmail.com>
2018-03-11 19:56:00 -0600

    Diseño interfaz inicial terminado

--- src/Ventanas/Gestor.java
+++ src/Ventanas/Gestor.java
@@ -10,9 +10,11 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.File;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -26,9 +28,11 @@ import javax.swing.JPanel;
 public class Gestor 
 {
    private final String Titulo="Invaders";
-   private final String Icono="";
-   private  Font FuenteTitulo;
+   private final Image Icono=Toolkit.getDefaultToolkit().getImage("Resources/Icono.png");
+   private final Image Back=Toolkit.getDefaultToolkit().getImage("Resources/Background.jpg");
+   private Font FuenteTitulo;
    private final Color Btn=new Color(75,0,130);
+   private final Color Trans= new Color(0,0,0);
     public Gestor()
     {
         try 
@@ -40,6 +44,8 @@ public class Gestor
         {
             FuenteTitulo = null;
         }
+        
+         
     }
 
 
@@ -69,37 +75,56 @@ public class Gestor
       JFrame Ventana = new JFrame(Titulo);
       Ventana.setResizable(false);
       Ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Ventana.setIconImage(Toolkit.getDefaultToolkit().getImage("Resources/Icono.png"));
-      Ventana.setSize(800,700);
+      Ventana.setIconImage(Icono);
+      Ventana.setSize(600,700);
       Ventana.setLocationRelativeTo(null);
-      Ventana.setBackground(Color.DARK_GRAY);
-
-      JPanel Uno= new JPanel();
-      Uno.setBackground(Color.DARK_GRAY);
-      JPanel Dos= new JPanel();
-      Dos.setBackground(Color.DARK_GRAY);
-      Dos.setLayout(new GridLayout(5,1));
+      
+      
+      JLabel Uno= new JLabel(new ImageIcon(Back));
+      Uno.setLayout(new BoxLayout(Uno,BoxLayout.Y_AXIS));
       
       JLabel Title= new JLabel("Invaders");
       Title.setFont(FuenteTitulo.deriveFont(Font.PLAIN,80));
       Title.setForeground(Color.GREEN);
+      Title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
       Uno.add(Title);
-      
+     
 
       JLabel Icon=new JLabel();
       Icon.setIcon(new ImageIcon(("Resources/Icono2.png")));
       Icon.setHorizontalAlignment(JLabel.CENTER);
-      Dos.add(Icon);
+      Icon.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+      Uno.add(Icon);
+      Uno.add(Box.createRigidArea(new Dimension(100,50)));
       
       JButton Jugar= new JButton("¡JUGAR!");
       Jugar.setFont(FuenteTitulo.deriveFont(Font.PLAIN,20));
       Jugar.setForeground(Btn);
       Jugar.setBackground(Color.GREEN);
-      Jugar.setPreferredSize(new Dimension(200,200));
-      Dos.add(Jugar);
+      Jugar.setAlignmentX(JButton.CENTER_ALIGNMENT);
+      Jugar.setMaximumSize(new Dimension(300,100));
+      Uno.add(Jugar);
+      Uno.add(Box.createRigidArea(new Dimension(100,50)));
       
-      Ventana.add(Uno,BorderLayout.NORTH);
-      Ventana.add(Dos,BorderLayout.CENTER);
+      JButton Estadísticas= new JButton("Estadisticas");
+      Estadísticas.setFont(FuenteTitulo.deriveFont(Font.PLAIN,20));
+      Estadísticas.setForeground(Btn);
+      Estadísticas.setBackground(Color.GREEN);
+      Estadísticas.setAlignmentX(JButton.CENTER_ALIGNMENT);
+      Estadísticas.setMaximumSize(new Dimension(300,100));
+      Uno.add(Estadísticas);
+      Uno.add(Box.createRigidArea(new Dimension(100,50)));
+      
+      JButton Salir= new JButton("Salir");
+      Salir.setFont(FuenteTitulo.deriveFont(Font.PLAIN,20));
+      Salir.setForeground(Btn);
+      Salir.setBackground(Color.GREEN);
+      Salir.setAlignmentX(JButton.CENTER_ALIGNMENT);
+      Salir.setMaximumSize(new Dimension(300,100));
+      Uno.add(Salir);
+      Uno.add(Box.createRigidArea(new Dimension(100,50)));
+      
+      Ventana.add(Uno,BorderLayout.CENTER);
       Ventana.setVisible(true);
     }
 }
