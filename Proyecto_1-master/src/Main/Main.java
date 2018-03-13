d03b0b15d01b8f95abbd03a8d98dbeb59753351a
dcamachog1501 <37154009+dcamachog1501@users.noreply.github.com>
2018-03-12 19:56:37 -0600

    Nueva Ventana

---
+++ Proyecto_1-master/src/Main/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Ventanas.Gestor;
+
+/**
+ *
+ * @author dcama
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       Gestor g = new Gestor();
+       g.GestWindow(2);
+        
+    }
+    
+}
